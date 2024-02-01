ccdb76ccd36cccf8fc7efb90a42dbfb5fef701c5
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-02-01 16:14:55 +0530

    Grafbase Model Setup

---
+++ graphbase/grafbase.config.ts
@@ -0,0 +1,32 @@
+import { graph, auth, config } from "@grafbase/sdk";
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+const g = graph.Standalone();
+
+// @ts-ignore
+const Project = g.model("Project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.ref(User),
+});
+
+// @ts-ignore
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 50 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.ref(Project).list().optional(),
+});
+
+export default config({
+  graph: g,
+});
